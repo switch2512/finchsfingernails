b309cb547c4f558ffaf0e95101bc12d00e717b06
switch2512 <gietzen.cameron@gmail.com>
2022-05-24 22:45:55 -0700

    Initial Commit on updates
    Got the base going.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/navbar'
+import Home from './components/home'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+    return (
+      <div class='navbar'>
+        <a><h2>Home</h2></a>
+        <a><h2>Pricing</h2></a>
+        <a><h2>Schedule</h2></a>
+        <a><h2>Gallery</h2></a>
+        <a><h2>Reviews</h2></a>
+      </div>
+    );
+  }
+  
+  export default Navbar;
