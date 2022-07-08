7620756d38eb449640f06026f58de2d6134df731
switch2512 <gietzen.cameron@gmail.com>
2022-07-07 20:46:25 -0600

    First changes

--- src/components/home.js
+++ src/components/home.js
@@ -17,18 +17,7 @@ function Home(props) {
     }
     return (
       <div class='home'>
-        <img src={require('../pics/finch-courtyard.jpg')} />
-        <div class='splash-title' style={{visibility: moveUpToggle[1]}}>
-          <h1>Testing</h1>
-        </div>
-        <button onClick={handleMoveUp}><strong>I'm a button</strong></button>
-        <div class="splash-flex" style={{transform: moveUpToggle[2], transition: '2s'}}>
-            <img src={require("../pics/balloon.png")}/>
-          <div class="hidden-splash">
-            <h2>Bio</h2>
-            <p><strong>Nicole is a woman and a college student. She currently works and studies. In her spare time Nicole likes to rip farts and pass gas to excess.</strong></p>
-          </div>
-        </div>
+        
       </div>
     );
   }
