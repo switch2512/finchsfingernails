fe012e698a3a664044cad654479b5fc323e4e707
switch2512 <gietzen.cameron@gmail.com>
2022-07-07 22:05:16 -0600

    Updates with Grace
    Got a solid start on the Home page

--- src/components/home.js
+++ src/components/home.js
@@ -17,7 +17,11 @@ function Home(props) {
     }
     return (
       <div class='home'>
-        
+        <div><img src={require('../pics/polish.jpg')} /></div>
+        <div class='text-div'>
+          <div><h1>BOOK TODAY!</h1></div>
+          <div class='subtext'><h2>FINCH'S NAILS OFFERS A VARIETY OF MANICURE SERVICES RIGHT FROM THE COMFORT OF YOUR OWN DORM</h2><p>NICOLE GIETZEN<br/>@instagram-handle</p></div>
+        </div>
       </div>
     );
   }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,8 +6,8 @@ function Navbar(props) {
     return (
       <div class='navbar'>
         <button onClick={props.handleHome} type="button"><h2>HOME</h2></button>
-        <button onClick={props.handlePricing} type="button"><h2>PRICING</h2></button>
         <button onClick={props.handleSchedule} type="button"><h2>SCHEDULE</h2></button>
+        <img src={require('../pics/logo7.png')} />
         <button onClick={props.handleGallery} type="button"><h2>GALLERY</h2></button>
         <button onClick={props.handleReviews} type="button"><h2>REVIEWS</h2></button>
       </div>
