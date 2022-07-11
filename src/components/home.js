25d140d31cbdc355940840856f3c96962258662e
switch2512 <gietzen.cameron@gmail.com>
2022-07-11 08:13:40 -0600

    updates

--- src/components/home.js
+++ src/components/home.js
@@ -17,10 +17,13 @@ function Home(props) {
     }
     return (
       <div class='home'>
-        <div><img src={require('../pics/polish.jpg')} /></div>
+        <div class='pic-div'><img src={require('../pics/polish.jpg')} /></div>
         <div class='text-div'>
-          <div><h1>BOOK TODAY!</h1></div>
-          <div class='subtext'><h2>FINCH'S NAILS OFFERS A VARIETY OF MANICURE SERVICES RIGHT FROM THE COMFORT OF YOUR OWN DORM</h2><p>NICOLE GIETZEN<br/>@instagram-handle</p></div>
+          <div class='subtext1'><h1>BOOK TODAY</h1></div>
+          <div class='subtext2'>
+            <h2>FINCH'S NAILS OFFERS A VARIETY OF MANICURE SERVICES RIGHT FROM THE COMFORT OF YOUR OWN DORM</h2>
+            <p>NICOLE GIETZEN<br/>@instagram-handle</p>
+          </div>
         </div>
       </div>
     );
--- src/components/pricing.js
+++ src/components/pricing.js
@@ -4,38 +4,15 @@ import ReactDOM from 'react-dom/client';
 function Pricing(props) {
     return (
       <div class='pricing'>
-        <img src={require('../pics/beautiful-point-loma.jpg')} />
-        <div>
-          <div class="splash-title">
-            <h1>Pricing</h1>
-          </div>
-          <div class="body">
-            <div class="title-row">
-              <h2>General Services</h2>
-            </div>
-            <div class="row">
-              <p>Single Color Gel: </p>
-              <p>$15</p>
-            </div>
-            <div class="odd">
-              <p>Tips with Gel:</p>
-              <p>$25</p>
-            </div>
-            <div class="row">
-              <br></br>
-            </div>
-            <div class="title-row2">
-              <h2>Additional Services</h2>
-            </div>
-            <div class="row">
-              <p>Gel Removal:</p>
-              <p>$5</p>
-            </div>
-            <div class="odd footer">
-              <p>Add Designs</p>
-              <p>$5</p>
-            </div>
-          </div>
+        <div class='price-container'>
+          <div class='price-item'><h1>General Services</h1></div>
+          <div class='price-item'><h1>Price</h1></div>
+          <div class='price-item'></div>
+          <div class='price-item'></div>
+          <div class='price-item'></div>
+          <div class='price-item'></div>
+          <div class='price-item'></div>
+          <div class='price-item'></div>
         </div>
       </div>
     );
